6646a30a478028ce574335c07abbd60a60a4e529
ratherforky <jf16688@bristol.ac.uk>
2024-06-28 15:38:21 +0100

    functional repl

--- src/WebRepl.hs
+++ src/WebRepl.hs
@@ -2,17 +2,27 @@
 module WebRepl where
 
 import Control.Monad.IO.Class
-import Data.Aeson
-import Data.ByteString
+-- import Data.Aeson
+import Data.ByteString.UTF8 (toString, fromString)
+import Flow
 import Network.HTTP.Req
-import Data.String (IsString(..))
+import Network.URI
 import StringMap
+import System.Console.Isocline (readline)
 
-eg :: String -> IO ByteString
+repl :: IO ()
+repl = do
+  msg <- readline "icfp"
+  putStrLn ""
+  putStrLn "Response:"
+  sendMessage msg
+  repl
+
+sendMessage :: String -> IO ()
 -- You can either make your monad an instance of 'MonadHttp', or use
 -- 'runReq' in any IO-enabled monad without defining new instances.
-eg message = runReq defaultHttpConfig $ do
-  let payload = fromString message
+sendMessage message = runReq defaultHttpConfig $ do
+  let payload = fromString ('S' : (toIcfpString message))
 
   -- One function—full power and flexibility, automatic retrying on timeouts
   -- and such, automatic connection sharing.
@@ -23,4 +33,10 @@ eg message = runReq defaultHttpConfig $ do
       (ReqBodyBs payload) -- use built-in options or add your own
       bsResponse -- specify how to interpret response
       (header "Authorization" "Bearer 3e6b791a-a998-49db-9ec6-ddc017323fc0") -- query params, headers, explicit port number, etc.
-  pure (responseBody r)
+  let responseMessage
+        = r |> responseBody
+            |> toString
+            |> Prelude.tail
+            |> fromIcfpString
+  liftIO $ print (r |> responseBody)
+  liftIO $ putStrLn responseMessage
