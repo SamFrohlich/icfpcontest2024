2808e871acaccb9ee85954293d0086d25a69cd2f
ratherforky <jf16688@bristol.ac.uk>
2024-06-28 13:54:55 +0100

    we have translation into english

--- src/StringMap.hs
+++ src/StringMap.hs
@@ -1,1 +1,15 @@
 module StringMap where
+
+import Data.Map (Map)
+import Data.Map qualified as M
+
+convertString :: String -> String
+convertString = map toICFPChar
+
+toICFPChar :: Char -> Char
+toICFPChar = (charMap M.!)
+
+charMap :: Map Char Char
+charMap = M.fromList
+  (zip ['!'.. '~']
+       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n")
