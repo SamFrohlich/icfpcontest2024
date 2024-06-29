395d3e2d0bfed4a0231740e8f7ba494d3c1a606d
Samantha Frohlich <sf16540@my.bristol.ac.uk>
2024-06-29 12:37:44 +0100

    parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,67 @@
 module Parser where
 
+-- base
+import Prelude hiding (Ordering(..))
+
+-- parsec
 import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Expr
+import Text.Parsec.Token
+
+-- local
+import AST
+import NumberMap
+import StringMap
+
+-- >>> parse uop "" "U-"
+-- Right Neg
+uop :: Parser UOp
+uop = char 'U' *>
+  (   try (Neg <$ char '-')
+  <|> try (Not <$ char '!')
+  <|> try (S2I <$ char '#')
+  <|> try (I2S <$ char '$')
+  <?> "uop")
+
+bop :: Parser BOp
+bop = char 'B' *>
+  (   try (Add <$ char '+')
+  <|> try (Sub <$ char '-')
+  <|> try (Mul <$ char '*')
+  <|> try (Div <$ char '/')
+  <|> try (Mod <$ char '%')
+  <|> try (LT  <$ char '<')
+  <|> try (GT  <$ char '>')
+  <|> try (EQ  <$ char '=')
+  <|> try (Or  <$ char '|')
+  <|> try (And <$ char '&')
+  <|> try (Cat <$ char '.')
+  <|> try (Take <$ char 'T')
+  <|> try (Drop <$ char 'D')
+  <|> (App <$ char '$')
+  <?> "bop")
+
+icfp :: Parser (ICFP Int)
+icfp =  buildExpressionParser table term
+
+-- leaves
+term =  try (T <$ char 'T')
+    <|> try (F <$ char 'F')
+    <|> try (char 'I' *> ((I . fromBase94) <$> manyTill anyChar (char ' ')) )
+    <|> try (char 'S' *> ((S . fromIcfpString) <$> manyTill anyChar (char ' ')) )
+    <|> try (char 'v' *> ((V . fromBase94) <$> manyTill anyChar (char ' ')) )
+
+-- ops
+table = [[ unary "U-" (U Neg)
+         , unary "U!" (U Not)
+         , unary "U#" (U S2I)
+         , unary "U$" (U I2S)
+        --  , binary "B+" (B Add)
+        -- TODO operations dont support binary prefix
+          ]]
+
+
+unary name fun = Prefix (do { try (string name); return fun })
+binary name fun = Prefix (do { try (string name); return fun })
+
